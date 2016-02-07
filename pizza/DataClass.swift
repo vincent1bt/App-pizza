d2986e833cee90d63aea2251a92767ca96295726
Vicente <vincent_97@live.com.mx>
2016-02-07 15:06:25 -0600

    App terminada

---
+++ pizza/DataClass.swift
@@ -0,0 +1,19 @@
+//
+//  DataClass.swift
+//  pizza
+//
+//  Created by vicente rodriguez on 1/31/16.
+//  Copyright © 2016 vicente rodriguez. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    var pizza : String = "Grande"
+    var masa : String = "Delgada"
+    var queso : String = "Mozarela"
+    var ingredientes = [Int: String]()
+}
+
+
+var info = Data()
